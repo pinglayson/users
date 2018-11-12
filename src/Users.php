e958207b817d4442e50471e1df015884feaa9a75
Ping Layson <ping.layson@gtdollar.com>
2018-11-12 07:06:31 +0000

    login function

--- src/Users.php
+++ src/Users.php
@@ -9,4 +9,13 @@ class Users{
   public function index(){
     return 'Hello World';
   }
+
+  /**
+   * Login via email and password
+   *
+   * @param $credentials array['email'=>'email@email.com', password => 'secret' ,'source' => 'web|mobile', locale=>'en|zh']
+   */
+  public function login($credentials){
+    var_dump($credentials);
+  }
 }
